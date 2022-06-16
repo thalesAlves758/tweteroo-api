2de225fdf765486ec354a43186d12bbf4a93fd44
Thales Alves <thalesalves758@gmail.com>
2022-06-16 16:45:56 -0300

    Add global variables

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
 app.use(cors());
+
+const users = [
+  {
+    username: 'bobesponja',
+    avatar:
+      'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info',
+  },
+];
+
+const tweets = [
+  {
+    username: 'bobesponja',
+    tweet: 'eu amo o hub',
+  },
+];
 
 export default app;
